d63a26b182c3b5353708b493aac413c55b324975
Nicolas Heine <nicolas.heine11@gmail.com>
2017-11-28 14:54:24 +0100

    test

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const keySecret = process.env.SECRET_KEY;
 
 const app = require("express")();
 const stripe = require("stripe")(keySecret);
-const path = require('path')
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.set("view engine", "pug");
 app.use(require("body-parser").urlencoded({extended: false}));
